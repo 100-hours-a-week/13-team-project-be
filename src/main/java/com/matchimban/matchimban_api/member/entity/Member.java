1f097437101d79e137c85fc7119004671d62bba3
Kimdonghyuk0 <kdh367036@naver.com>
2026-01-22 17:50:20 +0900

    feat: member 도메인 및 카카오 OAuth DB 연동(#2)

--- src/main/java/com/matchimban/matchimban_api/member/entity/Member.java
+++ src/main/java/com/matchimban/matchimban_api/member/entity/Member.java
@@ -13,13 +13,17 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "members")
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
 
 	@Id
