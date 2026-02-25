b4783b1d0b98d9b5011091a3c6cc7c4a6da46a95
redjin <hongjin914@gmail.com>
2026-02-25 19:11:44 +0900

    fix: make S3Presigner startup less sensitive to AWS provider bean creation

--- src/main/java/com/matchimban/matchimban_api/global/aws/S3PresignerConfig.java
+++ src/main/java/com/matchimban/matchimban_api/global/aws/S3PresignerConfig.java
@@ -1,11 +1,12 @@
 package com.matchimban.matchimban_api.global.aws;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.regions.providers.AwsRegionProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -14,11 +15,13 @@ public class S3PresignerConfig {
 
     @Bean(destroyMethod = "close")
     @ConditionalOnMissingBean(S3Presigner.class)
-    public S3Presigner s3Presigner(AwsCredentialsProvider credentialsProvider,
-                                   AwsRegionProvider regionProvider) {
+    public S3Presigner s3Presigner(
+            @Value("${spring.cloud.aws.region.static:ap-northeast-2}") String region
+    ) {
         return S3Presigner.builder()
-                .credentialsProvider(credentialsProvider)
-                .region(regionProvider.getRegion())
+                // Default chain is resolved lazily at call time, so startup doesn't fail in CI test env.
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of(region))
                 .build();
     }
 }
