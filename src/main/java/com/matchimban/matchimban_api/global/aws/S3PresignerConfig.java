038e98dae3329fd984c9402225611f2f03cac10d
redjin <hongjin914@gmail.com>
2026-02-25 18:28:18 +0900

    test: exclude S3Presigner config in test profile and add test stub

--- src/main/java/com/matchimban/matchimban_api/global/aws/S3PresignerConfig.java
+++ src/main/java/com/matchimban/matchimban_api/global/aws/S3PresignerConfig.java
@@ -3,11 +3,13 @@ package com.matchimban.matchimban_api.global.aws;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.providers.AwsRegionProvider;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
+@Profile("!test")
 public class S3PresignerConfig {
 
     @Bean(destroyMethod = "close")
